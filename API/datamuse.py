3cba399bc99b801d2f587495a48467e400e10821
Roshan Rajan <rjrajan2@illinois.edu>
2017-06-23 21:03:46 -0500

    Generates random words and gives rhymes

--- API/datamuse.py
+++ API/datamuse.py
@@ -1,21 +1,46 @@
 
 
-import urllib, json
+import urllib, json, random
 
 def read_jsonURL(jsonURL):
 	url_response = urllib.urlopen(jsonURL)
 	data = json.loads(url_response.read())
 	return data
 
-rhyme_url = "https://api.datamuse.com/words?rel_rhy="
-curr_word = "forgetful"
-curr_url = rhyme_url + curr_word
-rhyme_data = read_jsonURL(curr_url)
 
-curr_dict = dict()
-for element in rhyme_data:
-	if ' ' not in element["word"]:
-		curr_dict[str(element["word"])] = len(str(element["word"]))
 
-print(curr_dict['fretful'])
+def find_rhymes(word):
+	rhyme_url = "https://api.datamuse.com/words?rel_rhy=" + str(word)
+	rhyme_data = read_jsonURL(rhyme_url)
+
+	rhyme_dict = dict()
+	for element in rhyme_data:
+		if ' ' not in element["word"]:
+			rhyme_dict[str(element["word"])] = len(str(element["word"]))
+	return rhyme_dict
+
+
+
+def get_word():
+	word_file = "/usr/share/dict/words"
+	word_list = open(word_file).read().splitlines()
+	return random.choice(word_list)
+
+
+def find_rhymable_word():
+	done = False
+	word = ""
+	while(not done):
+		word = get_word()
+		rhyme_dict = find_rhymes(word)
+		done = bool(rhyme_dict)
+	return word, rhyme_dict			
+
+
+
+curr_word, curr_rhymes_dict = find_rhymable_word()
+print(curr_word)
+print(curr_rhymes_dict)
+
+
 
